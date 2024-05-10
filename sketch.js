a7e7eab959a3129890ad0ed4d4c5bcdd84cb27e0
Shivum Banerjee <73792393+ShivumB@users.noreply.github.com>
2024-05-10 11:15:21 -0500

    Simulating current - B field calculation broken

--- sketch.js
+++ sketch.js
@@ -1,16 +1,11 @@
-var wires = [];
-var pointList = [];
+var nodes = [];
 
 var batteries = [];
 var batterySprite;
 
-var keyReset = true;
-
 var sel = 0;
 
 var field = 0;
-
-var t = 0;
 
 function preload() {
   batterySprite = loadImage("battery.png");
@@ -26,11 +21,6 @@ function draw() {
 
   background(237, 181, 37);
 
-  if(keyReset && key == "") {
-    wires.splice(wires.length - 1, 1);
-    keyReset = false;
-  }
-
   if(field < 0) {
     fill(0);
     noStroke();
@@ -41,52 +31,14 @@ function draw() {
     line(width/2 - 5, height/2 - 5, width/2 + 5, height/2 + 5);
     line(width/2 + 5, height/2 - 5, width/2 - 5, height/2 + 5);
   }
-
+  
+  for(let i = 0; i < nodes.length; i++) {
+    nodes[i].update(batteries);
+    nodes[i].show();
+  }
+  
   for(let i = 0; i < batteries.length; i++)
     batteries[i].show();
-
-  strokeWeight(2);
-  for(let i = 0; i < wires.length; i++) {
-    if(wires[i][1]) stroke(255,0,0);
-    else stroke(0);
-
-    for(let j = 1; j < wires[i][0].length; j++) {
-      line(wires[i][0][j-1][0], wires[i][0][j-1][1], wires[i][0][j][0], wires[i][0][j][1]);
-    }
-  }
-
-  noStroke();
-  for(let i = 0; i < wires.length; i++) {
-
-    for(let j = 1; j < wires[i][0].length; j++) {
-      if(wires[i][1]) {
-        fill(255,0,0);
-        ellipse(wires[i][0][0][0], wires[i][0][0][1], 5, 5);
-        ellipse(wires[i][0][wires[i][0].length - 1][0], wires[i][0][wires[i][0].length - 1][1], 5, 5);
-
-        if(wires[i][2] == 1) {
-          ellipse(wires[i][0][j-1][0] + t * (wires[i][0][j][0] - wires[i][0][j-1][0]),
-          wires[i][0][j-1][1] + t * (wires[i][0][j][1] - wires[i][0][j-1][1]), 5, 5);
-        } else {
-          ellipse(wires[i][0][j][0] + t * (wires[i][0][j-1][0] - wires[i][0][j][0]),
-          wires[i][0][j][1] + t * (wires[i][0][j-1][1] - wires[i][0][j][1]), 5, 5);
-        }
-      } else {
-        fill(0);
-        ellipse(wires[i][0][0][0], wires[i][0][0][1], 5, 5);
-        ellipse(wires[i][0][j][0], wires[i][0][j][1], 5, 5);
-      }
-    }
-  }
-  t += 0.05;
-  t %= 1;
-
-  stroke(255);
-  fill(255);
-  for(let i = 1; i < pointList.length; i++) {
-    line(pointList[i-1][0], pointList[i-1][1], pointList[i][0], pointList[i][1]);
-    ellipse(pointList[i][0], pointList[i][1], 5, 5);
-  }
 
   textAlign(CENTER, TOP);
   textSize(25);
@@ -116,78 +68,26 @@ function mouseClicked() {
   if(mouseX < width/16) {
     sel = Math.floor(mouseY / (height/6));
   } else {
-
     if(sel == 1) {
       batteries.push(new Battery(mouseX - 37/2, mouseY - 69/2));
-
-      checkCircuits(batteries, wires);
-      field = calcBiotSavart(wires);
     }
 
   }
 }
 
 function mouseDragged() {
-  if(sel == 0 && mouseX > width/16) {
-    pointList.push([mouseX, mouseY]);
-  }
-}
-
-function mouseReleased() {
-
-  if(sel == 0 && pointList.length > 0) {
-    wires.push([pointList, false, 1]);
-    pointList = [];
-
-    checkCircuits(batteries, wires);
-    field = calcBiotSavart(wires);
-  }
-}
-
-function calcBiotSavart(wires) {
-
-  let current = 1;
-  let field = 0;
-
-  let deltaL = [];
-  let deltaR = [];
-  let crossProd = 0;
-  let rMag = 0;
-  
-  for(let i = 0; i < wires.length; i++) {
-
-    if(!wires[i][1]) continue;
+  if(sel == 0) {
     
-    //SIGMA of delta B
-    for(let j = 1; j < wires[i][0].length; j++) {
-
-      deltaL = [wires[i][0][j][0] - wires[i][0][j-1][0], wires[i][0][j][1] - wires[i][0][j-1][1]];
-      deltaR = [width/2 - (wires[i][0][j][0] + wires[i][0][j-1][0])/2, height/2 - (wires[i][0][j][1] + wires[i][0][j-1][1])/2];
-
-      rMag = deltaR[0] * deltaR[0] + deltaR[1]*deltaR[1];
-
-      crossProd = deltaL[0] * deltaR[1] - deltaL[1] * deltaR[0];
-      crossProd /= Math.sqrt(rMag)
-
-      field += crossProd / (rMag) * wires[i][1];
+    if(sel == 0) {
+      nodes.push(new Node(mouseX, mouseY));
+      
+      for(let i = 0; i < nodes.length - 1; i++) {
+        nodes[i].checkNode(nodes[nodes.length - 1]);
+        nodes[nodes.length - 1].checkNode(nodes[i]);
+      }
+      
     }
-    
-    //add the segment between last and first point! important.
-    deltaL = [wires[i][0][0][0] - wires[i][0][wires.length-1][0], wires[i][0][0][1] - wires[i][0][wires.length-1][1]];
-    
-    deltaR = [width/2 - (wires[i][0][0][0] + wires[i][0][wires.length-1][0])/2, height/2 - (wires[i][0][0][1] + wires[i][0][wires.length-1][1])/2];
-    rMag = deltaR[0] * deltaR[0] + deltaR[1]*deltaR[1];
-
-    crossProd = deltaL[0] * deltaR[1] - deltaL[1] * deltaR[0];
-    crossProd /= Math.sqrt(rMag)
-
-    field += crossProd / (rMag) * wires[i][1];
-
   }
-
-  //divides by myu * I / 4PI, the 4PI cancels out, so 10^-7 * I
-  return field * 0.0000001 * current;
-
 }
 
 class Battery {
@@ -198,50 +98,94 @@ class Battery {
   }
 
   show() {
+    noFill();
+    stroke(0);
+    strokeWeight(2);
     image(batterySprite, this.x, this.y, 37, 69);
   }
 }
 
-function keyPressed() {
-  keyReset = true;
-}
-
-function checkCircuits(batteries, wires) {
-
-  //set all wires false until they r checked
-  for(let i = 0; i < wires.length; i++) wires[i][1] = false;
-
-  for(let i = 0; i < batteries.length; i++) {
-
-    for(let j = 0; j < wires.length; j++) {
-
-      //starts at positive terminal, ends at negative
-      if(wires[j][0][0][0] > batteries[i].x && wires[j][0][0][0] < batteries[i].x + 37
-        && wires[j][0][0][1] > batteries[i].y && wires[j][0][0][1] < batteries[i].y + 30
-      && wires[j][0][wires[j][0].length - 1][0] > batteries[i].x
-      && wires[j][0][wires[j][0].length - 1][0] < batteries[i].x + 37
-      && wires[j][0][wires[j][0].length - 1][1] > batteries[i].y + 30
-      && wires[j][0][wires[j][0].length - 1][1] < batteries[i].y + 69) {
-
-        wires[j][1] = true;
-        wires[j][2] = 1;
-
-      }
-
-      //starts at negative terminal, ends at positive
-      else if(wires[j][0][0][0] > batteries[i].x && wires[j][0][0][0] < batteries[i].x + 37
-        && wires[j][0][0][1] > batteries[i].y + 30 && wires[j][0][0][1] < batteries[i].y + 69
-      && wires[j][0][wires[j][0].length - 1][0] > batteries[i].x
-      && wires[j][0][wires[j][0].length - 1][0] < batteries[i].x + 37
-      && wires[j][0][wires[j][0].length - 1][1] > batteries[i].y
-      && wires[j][0][wires[j][0].length - 1][1] < batteries[i].y + 30) {
-
-        wires[j][1] = true;
-        wires[j][2] = -1;
-      }
-
-    }
-
+class Node {
+  
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    
+    this.t = 0;
+    
+    this.full = 0;
+    
+    this.nodes = [];
   }
-
+  
+  checkNode(node) {
+    
+    if((node.x - this.x) * (node.x - this.x) + (node.y - this.y) * (node.y - this.y) < 400) {
+      this.nodes.push(node);
+    }
+  }
+  
+  show() {
+    
+    noStroke();
+    
+    
+    if(this.full == 0) fill(255, 0, 0, 100);
+    else fill (0, 255, 255, 100);
+    ellipse(this.x, this.y, 20, 20);
+    
+    if(this.full == 0) fill(0);
+    else fill(0, 0, 255);
+    ellipse(this.x, this.y, 5, 5);
+    
+    
+  }
+  
+  update(batteries) {
+    
+    this.t++;
+    this.t %= 10;
+    if(this.t != 0) return;
+    
+    if(this.full == 0) {
+      
+      for(let i = 0; i < batteries.length; i++) {
+        //receive from electron from positive terminal
+        if(this.x > batteries[i].x && this.x < batteries[i].x + 37 && this.y > batteries[i].y && this.y < batteries[i].y + 30) {
+          this.full = 1;
+          break;
+        }
+      }
+      
+    } else {
+      
+      //donate to negative terminal
+      for(let i = 0; i < batteries.length; i++) {
+        //check collision with positive terminal
+        if(this.x > batteries[i].x && this.x < batteries[i].x + 37 && this.y > batteries[i].y + 30 && this.y < batteries[i].y + 69) {
+          this.full = 0;
+          break;
+        }
+      }
+      
+      if(this.full == 0) return;
+      
+      let recipients = [];
+      //first, get indices of available recipients
+      for(let i = 0; i < this.nodes.length; i++)
+        if(this.nodes[i].full == 0) recipients.push(i);
+      
+      if(recipients.length > 0) {
+        
+        //then choose randomly
+      let chosen = recipients[Math.floor(Math.random() * recipients.length)];
+    
+        //then donate
+        this.nodes[chosen].full = 1;
+        this.full = 0;
+      }
+    }
+    
+  }
+  
 }
